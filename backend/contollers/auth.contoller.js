ff851b7e07934efac9f86ca90fbf6c3d5af306c8
jacobsanosh <jacobsanosh@gmail.com>
2024-08-17 02:29:17 +0530

    jwt token setting up

--- backend/contollers/auth.contoller.js
+++ backend/contollers/auth.contoller.js
@@ -1,9 +1,42 @@
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import generateJWTtoken from '../utils/generateJWTtoken.js';
+export const signupUser=async(req,res)=>{
+    try{
+
+        const  {fullName,username,password,confirmPassword,gender}=req.body; 
+        if (password!=confirmPassword){
+            return res.status(400).json({message:"Password and Confirm Password do not match"})
+        }
+        const userExist=await User.findOne({username});
+        if(userExist){
+            return res.status(400).json({message:"User already exists"});
+        }
+        const boyPics=`https://avatar.iran.liara.run/public/boy/?username=${username}`;
+        const girlPics=`https://avatar.iran.liara.run/public/girl/?username=${username}`;
+        const salt=await bcrypt.genSalt(10);
+        const hashpassword=await bcrypt.hash(password,salt);
+        const new_user=await User.create({
+            fullName,
+            username,
+            password:hashpassword,
+            gender,
+            profilePic:gender==='male' ? boyPics : girlPics
+        });
+        // for generating cookies
+        generateJWTtoken(new_user._id,res);
+        await new_user.save();
+        return res.status(201).json({_id:new_user._id,fullName:new_user.fullName,username:new_user.username,profilePic:new_user.profilePic});   
+    }
+    catch(err){
+        res.status(500).json({message:"Internal Server Error "});
+        console.log('error during signup controller:',err);
+    }
+
+}
 export const loginUser=(req,res)=>{
     res.send("on login page")
 }
 export const logoutUser=(req,res)=>{
     res.send("on logout page")
-}
-export const signupUser=(req,res)=>{
-    res.send("on signup page")
 }
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routers/auth.route.js';
+import { connect } from 'http2';
+import connectToMongoDB from './db/connectToMogoDB.js';
 dotenv.config();
 const port=process.env.PORT||3000;
 const app=express();
+app.use(express.json());
+app.use('/api/auth',authRoutes)
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
 // for user login
-app.use('/api/auth',authRoutes)
 app.listen(port,()=>{
+    connectToMongoDB();
     console.log(`Server is running on port ${port}`);
 });
