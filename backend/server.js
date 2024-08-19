81255de61b13af1438a89f4b7311734b4bd5e446
jacobsanosh <jacobsanosh@gmail.com>
2024-08-19 22:00:48 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from 'express';
 import {app,server} from './socket/socket.js';
 import dotenv from 'dotenv';
@@ -12,6 +13,7 @@ import connectToMongoDB from './db/connectToMogoDB.js';
 
 dotenv.config();
 const port=process.env.PORT||3000;
+const __dirname = path.resolve();
 
 // setting middlewares
 app.use(express.json());
@@ -20,6 +22,11 @@ app.use(cookieParser());
 app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes)
 app.use('/api/users',userRoutes)
+
+app.use(express.static(path.join(__dirname,'frontend/dist')));
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'frontend','dist','index.html'));
+})
 
 app.get('/',(req,res)=>{
     res.send('Hello World');
